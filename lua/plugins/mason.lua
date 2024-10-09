a734405426be82a60d236d16fb974af2296db994
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2024-10-08 22:45:18 -0300

    att

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -32,7 +32,38 @@ return {
     opts = {
       ensure_installed = {
         "python",
+        "codelldb",
         -- add more arguments for adding more debuggers
+      },
+      handlers = {
+        codelldb = function(source_name)
+
+          local dap = require "dap"
+
+          dap.configurations.c = {
+            {
+              name = "Launch file",
+              type = "codelldb",
+              request = "launch",
+              program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+              cwd = "${workspaceFolder}",
+              stopOnEntry = false,
+            },
+            {
+              name = "Compile and debug",
+              type = "codelldb",
+              request = "launch",
+              program = function()
+                local directory = vim.fn.expand "%:h"
+                local filename = vim.fn.expand "%:r"
+                vim.fn.system("gcc -fdiagnostics-color=always -g " .. directory .. "/*.c -o " .. filename .. " -lm")
+                return vim.fn.input("Path to executable: ", directory .. "/", "file")
+              end,
+              cwd = "${workspaceFolder}",
+              stopOnEntry = false,
+            },
+          }
+        end,
       },
     },
   },
